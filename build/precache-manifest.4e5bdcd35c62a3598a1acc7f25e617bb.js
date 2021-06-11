665de06ed8b63cc6528daf33bc8a4abd601c4401
ayan20ok <43269356+ayan20ok@users.noreply.github.com>
2021-06-11 14:13:41 +0530

    Add files via upload

---
+++ build/precache-manifest.4e5bdcd35c62a3598a1acc7f25e617bb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d40f403fb09faaefe6a258746111d2",
+    "url": "/%7Bayan-online%7D/index.html"
+  },
+  {
+    "revision": "ffa99de795a12413fdf9",
+    "url": "/%7Bayan-online%7D/static/css/2.afd40c00.chunk.css"
+  },
+  {
+    "revision": "19d510b8fd63191c99c7",
+    "url": "/%7Bayan-online%7D/static/css/main.8a3d94dd.chunk.css"
+  },
+  {
+    "revision": "ffa99de795a12413fdf9",
+    "url": "/%7Bayan-online%7D/static/js/2.0952ba67.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/%7Bayan-online%7D/static/js/2.0952ba67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d510b8fd63191c99c7",
+    "url": "/%7Bayan-online%7D/static/js/main.150a0091.chunk.js"
+  },
+  {
+    "revision": "8eb871f8494664a8ca64",
+    "url": "/%7Bayan-online%7D/static/js/runtime-main.6a4b297e.js"
+  },
+  {
+    "revision": "69dacb3d71093fc26d664657936186da",
+    "url": "/%7Bayan-online%7D/static/media/ayan_big.69dacb3d.jpg"
+  },
+  {
+    "revision": "efec513edb965f8c2363de7d88f722a1",
+    "url": "/%7Bayan-online%7D/static/media/globallogic_logo.efec513e.jpg"
+  },
+  {
+    "revision": "a240b3d19406a9fd7ee59f3a70056cf3",
+    "url": "/%7Bayan-online%7D/static/media/tis-logo.a240b3d1.jpg"
+  }
+]);
