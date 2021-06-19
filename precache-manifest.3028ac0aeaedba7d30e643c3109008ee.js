10be36fa45059c3ec08904922c6402d36a188257
Ayan Roy <ayan20ok@gmail.com>
2021-06-19 11:10:16 +0530

    Updates

---
+++ precache-manifest.3028ac0aeaedba7d30e643c3109008ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08b3dda3083222818980bbaea9aa744",
+    "url": "/ayan-online/index.html"
+  },
+  {
+    "revision": "ffa99de795a12413fdf9",
+    "url": "/ayan-online/static/css/2.afd40c00.chunk.css"
+  },
+  {
+    "revision": "259303f914818ba5a0f4",
+    "url": "/ayan-online/static/css/main.07147a41.chunk.css"
+  },
+  {
+    "revision": "ffa99de795a12413fdf9",
+    "url": "/ayan-online/static/js/2.0952ba67.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ayan-online/static/js/2.0952ba67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "259303f914818ba5a0f4",
+    "url": "/ayan-online/static/js/main.9ceeeae6.chunk.js"
+  },
+  {
+    "revision": "57a34b24e9b2cb1f8ba8",
+    "url": "/ayan-online/static/js/runtime-main.92039837.js"
+  },
+  {
+    "revision": "69dacb3d71093fc26d664657936186da",
+    "url": "/ayan-online/static/media/ayan_big.69dacb3d.jpg"
+  },
+  {
+    "revision": "efec513edb965f8c2363de7d88f722a1",
+    "url": "/ayan-online/static/media/globallogic_logo.efec513e.jpg"
+  },
+  {
+    "revision": "a240b3d19406a9fd7ee59f3a70056cf3",
+    "url": "/ayan-online/static/media/tis-logo.a240b3d1.jpg"
+  }
+]);
